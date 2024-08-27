fd0998c30cf2709acdb00079642741df56dde8dc
Alexander La Barge <labargeam@gmail.com>
2024-08-27 15:52:28 +0000

    ss

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+sudo apt-get install wget curl git vim btop -y
+cd /mnt/gentoo
+wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20240825T170406Z/stage3-amd64-desktop-systemd-20240825T170406Z.tar.xz
