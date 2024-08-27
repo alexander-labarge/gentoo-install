2ede015dc5506f86d6dce12f86803cc55fde26cb
Alexander La Barge <labargeam@gmail.com>
2024-08-27 11:58:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 sudo apt-get install wget curl git vim btop -y
 cd /mnt/gentoo
 wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20240825T170406Z/stage3-amd64-desktop-systemd-20240825T170406Z.tar.xz
+
+sudo tar xpf ./stage3-amd64-desktop-systemd-20240825T170406Z.tar.xz -C /mnt/gentoo --xattrs --numeric-owner
