c6a240beef553153305b6bd346aec335d236f3cb
Alexander La Barge <labargeam@gmail.com>
2024-08-27 20:35:30 +0000

    ff

--- install.sh
+++ install.sh
@@ -194,6 +194,7 @@ emerge --ask net-wireless/iw net-wireless/wpa_supplicant
 emerge --ask --verbose net-misc/networkmanager
 emerge --ask --verbose sys-boot/grub
 grub-install --efi-directory=/efi
+#grub-install --target=x86_64-efi --efi-directory=/efi --removable
 emerge --ask app-text/tree
 systemctl disable systemd-networkd-wait-online.service 
 systemctl enable NetworkManager
