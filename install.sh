498a12c5044b07fa76aa7790510ec1d4ef6b6adf
Alexander La Barge <labargeam@gmail.com>
2024-08-27 12:13:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,11 @@ cd /mnt/gentoo
 wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20240825T170406Z/stage3-amd64-desktop-systemd-20240825T170406Z.tar.xz
 sudo tar xpf ./stage3-amd64-desktop-systemd-20240825T170406Z.tar.xz -C /mnt/gentoo --xattrs --numeric-owner
 sudo cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+sudo mount --types proc /proc /mnt/gentoo/proc
+sudo mount --rbind /sys /mnt/gentoo/sys
+sudo mount --make-rslave /mnt/gentoo/sys
+sudo mount --rbind /dev /mnt/gentoo/dev
+sudo mount --make-rslave /mnt/gentoo/dev
+sudo mount --bind /run /mnt/gentoo/run
+sudo mount --make-slave /mnt/gentoo/run 
